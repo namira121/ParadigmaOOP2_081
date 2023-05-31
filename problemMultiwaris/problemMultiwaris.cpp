45a853d4d46881d3bc4231a7ca37e8feced74b3c
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-31 11:10:36 +0700

    pembetulan commit sebelumnya : membuat pekerja yang menjadi derived class dari orang yang menjadi base class

--- problemMultiwaris/problemMultiwaris.cpp
+++ problemMultiwaris/problemMultiwaris.cpp
@@ -13,5 +13,16 @@ public:
 };
 
 class pekerja : public orang {
+public:
 
+	pekerja(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pekerja dibuat\n" << endl;
+	}
+};
+
+class pelajar : public orang {
+public:
+	
 };
